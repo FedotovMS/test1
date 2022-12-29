74a17c1f0e8cfe1d7f01f6995e6650abe876f35b
Mykola Fedotov <fedotovns@gmail.com>
2022-12-29 11:05:28 +0100

    code1

---
+++ js/homework.js
@@ -0,0 +1,13 @@
+// const array = [1, 2, 3, 4, 5];
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+
+//   const firstElement = array[0];
+//   const lastElementIndex = array.length - 1;
+//   const lastElement = array[lastElementIndex];
+//   array = [firstElement, lastElement];
+//   console.log(array);
+//   // Change code above this line
+// }
+// getExtremeElements(array);
