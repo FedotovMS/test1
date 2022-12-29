e8310c16b9f0b6690751324d944fbdaa4ac19c09
Mykola Fedotov <fedotovns@gmail.com>
2022-12-29 21:57:01 +0100

    2

--- js/homework.js
+++ js/homework.js
@@ -1,5 +1,3 @@
-// const array = [1, 2, 3, 4, 5];
-
 // function getExtremeElements(array) {
 //   // Change code below this line
 
@@ -7,7 +5,44 @@
 //   const lastElementIndex = array.length - 1;
 //   const lastElement = array[lastElementIndex];
 //   array = [firstElement, lastElement];
-//   console.log(array);
+
+//   let result = array;
+//   console.log(result);
 //   // Change code above this line
 // }
-// getExtremeElements(array);
+// getExtremeElements(["apple", "peach", "palm", "banana"]);
+// getExtremeElements([1, 2, 3, 4, 5]);
+
+// const message = "Web-development is creative work";
+// let words = message.split(" ");
+// const wordsQ = words.length;
+// console.log(words);
+// console.log(wordsQ);
+// const price = 10;
+// let summ = wordsQ * price;
+// console.log(summ);
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Change code below this line
+//   let words = message.split(" ");
+
+//   const wordsQuantity = words.length;
+//   let summ = pricePerWord * wordsQuantity;
+
+//   console.log(summ);
+//   // Change code above this line
+// }
+// calculateEngravingPrice("JavaScript is in my blood", 10);
+
+function slugify(title) {
+  // Change code below this line
+
+  // title = title.split(" ");
+  // title = title.join("-");
+  console.log(slug);
+  // let slug = title.join("-");
+  // console.log(slug);
+
+  // Change code above this line
+}
+slugify("Arrays for begginers");
